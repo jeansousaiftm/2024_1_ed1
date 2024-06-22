5a647908f08feb02f32defc70ba0e7dd4bafb438
jeansousaiftm <61210073+jeansousaiftm@users.noreply.github.com>
2024-06-21 21:58:53 -0300

    Add files via upload

---
+++ 20240621_lista.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+struct No {
+
+    int valor;
+    No* prox;
+
+    No() {
+        prox = NULL;
+    }
+
+    No(int _valor) {
+        valor = _valor;
+        prox = NULL;
+    }
+
+};
+
+struct Lista {
+
+    No* inicio;
+    No* fim;
+
+    Lista() {
+        inicio = NULL;
+        fim = NULL;
+    }
+
+    void inserirInicio(int n) {
+        No* novo = new No(n);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            novo->prox = inicio;
+            inicio = novo;
+        }
+    }
+
+    void inserirFinal(int n) {
+        No* novo = new No(n);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim->prox = novo;
+            fim = novo;
+        }
+    }
+
+    void imprimir() {
+        No* aux = inicio;
+        //printf("%d %d\n", aux, aux->valor);
+        while (aux != NULL) {
+            printf("%d\n", aux->valor);
+            //printf("%d %d\n", aux->prox, aux->valor);
+            aux = aux->prox;
+        }
+    }
+
+};
+
+int main() {
+
+    Lista l;
+
+    l.inserirInicio(10);
+    l.inserirInicio(5);
+    l.inserirInicio(1);
+    l.inserirInicio(8);
+    l.inserirFinal(7);
+    l.inserirFinal(3);
+
+    l.imprimir();
+
+
+    return 0;
+}
