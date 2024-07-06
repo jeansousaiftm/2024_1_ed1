2af016e3a20a2dd9840f00a4ec3e4d1ee0f3a3aa
jeansousaiftm <61210073+jeansousaiftm@users.noreply.github.com>
2024-07-05 21:44:35 -0300

    Add files via upload

---
+++ 20240705_lista.cpp
@@ -0,0 +1,116 @@
+#include<stdio.h>
+
+struct No {
+
+    int valor;
+    No* prox;
+
+    No() {
+        prox = NULL;
+    }
+
+    No(int _valor) {
+        valor = _valor;
+        prox = NULL;
+    }
+
+};
+
+struct Lista {
+
+    No* inicio;
+    No* fim;
+    int n;
+
+    Lista() {
+        inicio = NULL;
+        fim = NULL;
+        n = 0;
+    }
+
+    void inserirInicio(int valor) {
+        No* novo = new No(valor);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            novo->prox = inicio;
+            inicio = novo;
+        }
+        n++;
+    }
+
+    void inserirFinal(int valor) {
+        No* novo = new No(valor);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim->prox = novo;
+            fim = novo;
+        }
+        n++;
+    }
+
+    void removerInicio() {
+        if (n == 0) return;
+        if (n == 1) {
+            delete(inicio);
+            inicio = NULL;
+            fim = NULL;
+            n--;
+            return;
+        }
+        No* aux = inicio;
+        inicio = inicio->prox;
+        delete(aux);
+        n--;
+    }
+
+    void removerFinal() {
+        if (n == 0) return;
+        if (n == 1) {
+            delete(inicio);
+            inicio = NULL;
+            fim = NULL;
+            n--;
+            return;
+        }
+        No* aux = inicio;
+        while (aux->prox != fim) {
+            aux = aux->prox;
+        }
+        delete(fim);
+        fim = aux;
+        aux->prox = NULL;
+        n--;
+    }
+
+    void imprimir() {
+        No* aux = inicio;
+        //printf("%d %d\n", aux, aux->valor);
+        while (aux != NULL) {
+            printf("%d\n", aux->valor);
+            //printf("%d %d\n", aux->prox, aux->valor);
+            aux = aux->prox;
+        }
+    }
+
+};
+
+int main() {
+
+    Lista l;
+
+    l.inserirInicio(10);
+    l.inserirInicio(5);
+    l.inserirInicio(1);
+    l.inserirInicio(8);
+    l.inserirFinal(7);
+    l.inserirFinal(3);
+
+    l.imprimir();
+
+
+    return 0;
+}
