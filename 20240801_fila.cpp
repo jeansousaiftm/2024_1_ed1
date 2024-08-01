43d8ed56749f187e4baa6a5503cfa38e7918fd15
jeansousaiftm <61210073+jeansousaiftm@users.noreply.github.com>
2024-08-01 20:12:43 -0300

    Add files via upload

---
+++ 20240801_fila.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+
+struct No {
+
+    int valor;
+    No* prox;
+
+    No() {
+        valor = 0;
+        prox = NULL;
+    }
+
+    No(int _valor) {
+        valor = _valor;
+        prox = NULL;
+    }
+
+};
+
+struct Fila {
+
+    No* inicio;
+    No* fim;
+    int n;
+
+    Fila() {
+        inicio = NULL;
+        fim = NULL;
+        n = 0;
+    }
+
+    void enfileirar(int valor) {
+        No* novo = new No(valor);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim->prox = novo;
+            fim = novo;
+        }
+        n++;
+    }
+
+    void desenfileirar(){
+        if (n == 0) return;
+        if (n == 1) {
+            delete(inicio);
+            inicio = NULL;
+            fim = NULL;
+            n = 0;
+            return;
+        }
+        No* aux = inicio;
+        inicio = inicio->prox;
+        delete(aux);
+        n--;
+    }
+
+    int frente() {
+        if (inicio == NULL) return 0;
+        return inicio->valor;
+    }
+
+};
+
+int main() {
+
+    Fila f;
+
+    f.enfileirar(10);
+    f.desenfileirar();
+    f.frente();
+
+    return 0;
+}
---
+++ 20240801_pilha.cpp
@@ -0,0 +1,69 @@
+#include<stdio.h>
+struct No {
+
+    int valor;
+    No* prox;
+
+    No() {
+        valor = 0;
+        prox = NULL;
+    }
+
+    No(int _valor) {
+        valor = _valor;
+        prox = NULL;
+    }
+
+};
+
+struct Pilha {
+
+    No* topo;
+    int n;
+
+    Pilha() {
+        topo = NULL;
+        n = 0;
+    }
+
+    void inserir(int valor) {
+        No* novo = new No(valor);
+        if (topo == NULL) {
+            topo = novo;
+        } else {
+            novo->prox = topo;
+            topo = novo;
+        }
+        n++;
+    }
+
+    void remover() {
+        if (n == 0) return;
+        if (n == 1) {
+            delete(topo);
+            topo = NULL;
+            n = 0;
+            return;
+        }
+        No* aux = topo;
+        topo = topo->prox;
+        delete(aux);
+        n--;
+    }
+
+    int topoPilha() {
+        if (topo == NULL) return 0;
+        return topo->valor;
+    }
+
+};
+
+int main() {
+
+    Pilha p;
+
+    p.inserir(10);
+    p.remover();
+
+    return 0;
+}
